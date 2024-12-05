82c39dacd1fc5f9a72beb35593c17ec7a2727990
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-12-05 13:43:03 +0100

    feat(global) new methode syncButtonsBottomRight
    the methode syncButtonsBottomRight is making button responsive

--- a_party_game_with_tower/global.gd
+++ a_party_game_with_tower/global.gd
@@ -2,11 +2,12 @@ extends Node
 
 @export var spawnBloc = true
 
-@export var cameraPositionY = 300
+@export var pieceSpeed = 100 #mock value
 
 @export var playingScene = "res://scenes/level/mainGame.tscn"
-@export var gamemodeScene = "res://scenes/gamemode/gamemode.tscn"
-@export var mainMenuScene = "res://scenes/menu/menu.tscn"
+@export var gamemodeScene = "res://scenes/menu/gamemode/gamemode.tscn"
+@export var mainMenuScene = "res://scenes/menu/homeMenu/menu.tscn"
+@export var parameterScene = "res://scenes/menu/parameter/parameter.tscn"
 
 @export var pieces  = [ #we can use it to define specific value like color/style
 	{
@@ -61,6 +62,27 @@ func syncMenuButtonsSize(buttonArray: Array):
 		
 		button.position.x = screen_size.x / 2 - width / 2
 		button.position.y = initial_y + index * (height + screen_size.y * 0.05)
+
+func syncButtonsBottomRight(buttonArray: Array):
+	var screen_size = get_viewport().size
+	
+	var width = screen_size.x * 0.2
+	var height = screen_size.y * 0.08
+	
+	var spacing = screen_size.y * 0.05
+	
+	var initial_y = screen_size.y - (buttonArray.size() * height + (buttonArray.size() - 1) * spacing)
+	
+	var initial_x = screen_size.x - width
+	
+	for index in range(buttonArray.size()):
+		var button = buttonArray[index]
+		
+		button.add_theme_font_size_override("font_size", screen_size.x / 50)
+		button.size = Vector2(width, height)
+		
+		button.position.x = initial_x
+		button.position.y = initial_y + index * (height + spacing)
 
 func syncMenuTitle(title: Label):
 	title.add_theme_font_size_override("font_size", get_viewport().size.x / 10)
