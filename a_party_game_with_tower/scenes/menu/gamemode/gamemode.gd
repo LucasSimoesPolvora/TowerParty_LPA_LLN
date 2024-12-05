0127551e29cb77678f31c1716e81302efe9271b8
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-12-05 13:34:34 +0100

    fix(new arborecense)
    move gamemode from :
    scense/menu -> scense/menu/gamemode

--- a_party_game_with_tower/scenes/menu/gamemode/gamemode.gd
+++ a_party_game_with_tower/scenes/menu/gamemode/gamemode.gd
@@ -4,11 +4,16 @@ var TitleLabel
 var LocalButton : Button
 var ChallengeButton : Button
 var BackButton : Button
+var screen_mode_button : Button
 var buttons = []
 
 const BACK_BUTTON_WIDTH_RATIO = 0.08
 const BACK_BUTTON_HEIGHT_RATIO = 0.08
 const BACK_BUTTON_POSITION_RATIO = 0.95
+const BUTTON_WIDTH_RATIO = 0.1
+const BUTTON_HEIGHT_RATIO = 0.08
+const BUTTON_POSITION_X_RATIO = 0.85
+const BUTTON_POSITION_Y_RATIO = 0.85
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,6 +21,7 @@ func _ready() -> void:
 	ChallengeButton = $challengeButton
 	TitleLabel = $title
 	BackButton = $backButton
+	screen_mode_button = $fullscreenButton
 	
 	buttons.append(LocalButton)
 	buttons.append(ChallengeButton)
@@ -63,6 +69,16 @@ func _on_back_button_pressed() -> void:
 func _on_fullscreen_button_pressed() -> void:
 	Global.change_screen_mode()
 	changeText()
+
+func change_button_font_size():
+	screen_mode_button.add_theme_font_size_override("font_size", get_viewport().size.y / 45)
+	
+	var width = get_viewport().size.x * BUTTON_WIDTH_RATIO
+	var heigth = get_viewport().size.y * BUTTON_HEIGHT_RATIO
+	screen_mode_button.size= Vector2(width, heigth)
+	
+	screen_mode_button.position.x = get_viewport().size.x * BUTTON_POSITION_X_RATIO
+	screen_mode_button.position.y = get_viewport().size.y * BUTTON_POSITION_Y_RATIO
 
 func changeText():
 	var buttonFullScreen = $fullscreenButton
