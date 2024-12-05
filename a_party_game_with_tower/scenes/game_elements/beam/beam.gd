a6dbc645d316228d22186899b7c6ba66055d39c5
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-12-05 13:41:50 +0100

    chores(beam) delete old print

--- a_party_game_with_tower/scenes/game_elements/beam/beam.gd
+++ a_party_game_with_tower/scenes/game_elements/beam/beam.gd
@@ -13,12 +13,7 @@ func updateBeam(fallingPiece, beam):
 		var coners_list = piece.get_node("border")
 		for corner in coners_list.get_children():
 			cpt += 1
-			print("Piece : ", piece.position)
-			print("Corner : ",cpt)
-			print("X,Y : ", corner.position)
 			var rotated_position = piece.to_global(corner.position)
-			print("Rotate X : ",rotated_position.x," Rotate Y : ",rotated_position.y)
-
 			if max_X == null or max_X < rotated_position.x:
 				max_X = rotated_position.x
 			if min_X == null or min_X > rotated_position.x:
@@ -27,4 +22,3 @@ func updateBeam(fallingPiece, beam):
 	beam.position.x = (min_X + max_X) / 2
 	beam.scale.x = abs(max_X - min_X) / beam.texture.get_width()
 	beam.scale.y = 20000
-	print("beam position : ", beam.position)
